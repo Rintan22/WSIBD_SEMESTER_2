22149f215d4bcf36346efa2137b715b736b55fdf
Rintan22 <nurwardanirintan@gmail.com>
2025-02-13 11:44:46 +0700

    Create WSIBDAcara5.java

---
+++ WSIBD acara 5/src/wsibd/acara/pkg5/WSIBDAcara5.java
@@ -0,0 +1,16 @@
+
+package wsibd.acara.pkg5;
+public class WSIBDAcara5 {
+
+    
+    public static void main(String[] args) {
+        String nama = "Lisa Polombo" ;
+        System.out.println("Student name: " + nama);
+        String stuld = "12345678" ;
+        System.out.println("Student ID: " + stuld);
+        String stuStatus = "Active" ;
+        System.out.println("Student Status: " + stuStatus);
+    }
+    // no 3
+    
+}
