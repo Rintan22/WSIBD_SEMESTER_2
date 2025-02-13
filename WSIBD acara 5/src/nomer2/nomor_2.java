51aa2dbbad8cdcb929e7274a46e05fdd70b0a0d5
Rintan22 <nurwardanirintan@gmail.com>
2025-02-13 11:53:55 +0700

    Create nomor_2.java

---
+++ WSIBD acara 5/src/nomer2/nomor_2.java
@@ -0,0 +1,38 @@
+package nomer2;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class nomor_2 {
+    
+      // Declare variables for the person's name and age
+    private String name;
+    private int age;
+
+    // Constructor to initialize the name and age
+    public nomor_2(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Method to return the person's name
+    public String getName() {
+        return name;
+    }
+
+    // Method to return the person's age
+    public int getAge() {
+        return age;
+    }
+
+    // Method to set the person's age
+    public void setAge(int age) {
+        this.age = age;
+}
+}
